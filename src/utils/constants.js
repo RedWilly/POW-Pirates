6c654c6d33302c51ef4ff9c50c1deef39aacee9c
RedWilly <thompsonau023@gmail.com>
2022-10-08 09:09:19 -0400

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Abi.json'
 
 export const contractABI = abi;
-export const contractAdress = "0xd510C8F2cE02fAe028fee4feB9Da11aE3342E440";
+export const contractAdress = "0x9fED227e39460614b41206CDb1cb37363393F60f";
