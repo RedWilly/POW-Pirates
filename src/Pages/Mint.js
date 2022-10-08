283433f69044af77a1bbac712a8d569b7b5709a6
RedWilly <thompsonau023@gmail.com>
2022-10-08 10:30:08 -0400

    Update Mint.js

--- src/Pages/Mint.js
+++ src/Pages/Mint.js
@@ -105,7 +105,7 @@ function Mint() {
                          <div className='Item'>
                            <h1>Price :</h1>
                            <h1 className='Round'>
-                           0.40 ETH
+                           0.40 ETHW
                            </h1>
                          </div>
                   </div>
